464163998cc06b3bd02f9eb6005a87fb99998362
Ana Juárez <lisa13suarez@gmail.com>
2022-08-23 22:40:13 -0600

    Agregando css de carrito

--- script/ejecuta.js
+++ script/ejecuta.js
@@ -38,7 +38,72 @@ const insertBuy = () => {
 */
 
 const areaHTML = document.querySelector('.container-carrito');
+const btnCompra = document.querySelectorAll('.buy');
 let infoProducts = [];
 let compras;
 
-console.log(areaHTML);
+console.log(btnCompra);
+
+btnCompra.forEach(addcardbtn => { 
+    addcardbtn.addEventListener('click', addtoCardClick);
+});
+
+function addtoCardClick(event) {
+    const button = event.target;
+    const item = button.closest('.tarjeta');
+    
+    const itemTitle = item.querySelector('.name-cactus').textContent;
+    const itemPrice = item.querySelector('.precio').textContent;
+    const itemImge = item.querySelector('img').src;
+
+    
+
+    addItemtoshopingCard(itemTitle, itemPrice, itemImge);
+}
+
+function addItemtoshopingCard(itemTitle, itemPrice, itemImge) {
+    console.log(itemTitle, itemPrice, itemImge);
+
+    const shopingCardRow = document.createElement('div');
+   /* const shoppingCardItemsContainer = document.querySelector('.mostrarcarrito');
+    console.log(shoppingCardItemsContainer);*/
+
+    const shopingCardContent = `
+    <div id="carrito-items">
+        <div id="items">
+            <p>Producto: ${itemTitle}</p>
+            <img src=${itemImge} width="120" height="100">
+            <p>Descripción: Planta Ordamental</p>
+            <p>Precio: </p>
+            <p class="precio">NIO ${itemPrice}</p>
+            <input class="num" type="number" value="1">
+            <p style="margin-top: -20px;">Cantidad: </p>
+            
+            <button id="boton-vaciar">Vaciar carrito</button>
+        </div>
+      </div>`;
+    
+      shopingCardRow.innerHTML = shopingCardContent;
+      areaHTML.append(shopingCardRow);
+
+      updateShoppingCardTotal();
+}
+
+function updateShoppingCardTotal() {
+    let total = 0;
+
+    const shoppingCardTotal = document.querySelector('.precio-total');
+    const shoppingCardItems = document.querySelectorAll('#carrito-items');
+    
+    shoppingCardItems.forEach(shoppingCardItem => {
+        const shopingCardItemsPriceElement = shoppingCardItem.querySelector('.precio');
+        const shopingCardItemPrice = Number(shopingCardItemsPriceElement.textContent.replace('NIO',''));
+
+        const shoppingCartItemQuantityElement = shoppingCardItem.querySelector('.num');
+        const shoppingCartItemQuantity = Number(shoppingCartItemQuantityElement.value);
+
+        total = total + shopingCardItemPrice * shoppingCartItemQuantity;
+    });
+
+    shoppingCardTotal.innerHTML = `NIO ${total}`;
+}
